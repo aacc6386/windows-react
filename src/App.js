c813d23f3282fd50041bc009f7db83d8f491643b
Stilic <lateman-jpeg@outlook.fr>
2021-08-24 14:49:44 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import * as React from "react";
-import { Rnd } from "react-rnd";
+import Window from "./Window";
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <>
-                <Rnd
-                    className="window"
-                    dragHandleClassName="title-bar"
-                    minWidth={250}
-                    minHeight={150}
-                >
-                    <div className="title-bar">
-                        <div className="title-bar-text">
-                            A Window With Stuff In It
-                        </div>
-                        <div className="title-bar-controls">
-                            <button aria-label="Minimize"></button>
-                            <button aria-label="Maximize"></button>
-                            <button aria-label="Close"></button>
-                        </div>
-                    </div>
-                    <div className="window-body">
-                        <p>There's so much room for activities!</p>
-                    </div>
-                </Rnd>
-            </>
-        );
-    }
+export default function App() {
+    return (
+        <>
+            <Window title="baka"><p>sus</p></Window>
+            <Window title={"sususus"}><p>bruh</p></Window>
+        </>
+    );
 }
---
+++ src/Window.js
@@ -0,0 +1,51 @@
+import { useRef, useState } from "react";
+import useDoubleClick from "use-double-click";
+import { Rnd } from "react-rnd";
+import "./Window.scss";
+
+export default function Window(props) {
+    const [maximized, setMaximize] = useState(false);
+    const titlebar = useRef();
+    const rhClasse = "resizeHandler" + (maximized ? " max" : "");
+    useDoubleClick({
+        onDoubleClick: (e) => {
+            setMaximize(!maximized);
+        },
+        ref: titlebar,
+    });
+    return (
+        <>
+            <Rnd
+                className={"window" + (maximized ? " maximized" : "")}
+                dragHandleClassName="title-bar"
+                enableResizing={!maximized}
+                disableDragging={maximized}
+                resizeHandleClasses={{
+                    bottom: rhClasse,
+                    bottomLeft: rhClasse,
+                    bottomRight: rhClasse,
+                    left: rhClasse,
+                    right: rhClasse,
+                    top: rhClasse,
+                    topLeft: rhClasse,
+                    topRight: rhClasse,
+                }}
+                minWidth={250}
+                minHeight={150}
+            >
+                <div className="title-bar" ref={titlebar}>
+                    <div className="title-bar-text">{props.title}</div>
+                    <div className="title-bar-controls">
+                        <button aria-label="Minimize"></button>
+                        <button
+                            aria-label="Maximize"
+                            onClick={() => setMaximize(!maximized)}
+                        ></button>
+                        <button aria-label="Close"></button>
+                    </div>
+                </div>
+                <div className="window-body">{props.children}</div>
+            </Rnd>
+        </>
+    );
+}
